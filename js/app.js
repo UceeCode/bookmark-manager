5176619458c8f4609c33dcd3b151111132daaad8
Frank.Lin <writefranklyn@gmail.com>
2023-12-30 18:42:42 +0100

    added  addBookmark javascript functionality

--- js/app.js
+++ js/app.js
@@ -2,3 +2,17 @@
  document.addEventListener('DOMContentLoaded', () => {
     loadBookmark();
  });
+
+ //function to add a bookmark
+ let addBookmark = () => {
+    let titleInput = document.getElementById('title');
+    let urlInput = document.getElementById('url');
+
+    let title = titleInput.value;
+    let url = urlInput.value;
+
+    if (!title || !url){
+        alert('Please enter both title and url');
+        return;
+    }
+ }
